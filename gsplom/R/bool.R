eaf998f98531b922d5ffb33b45e19e619d7ad20d
Andrew Yates <andrewyates.name@gmail.com>
2014-03-11 00:12:48 -0700

    logical class

---
+++ gsplom/R/bool.R
@@ -0,0 +1,102 @@
+##########################################################################
+# Copyright (c) 2014 Andrew Yates
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+##########################################################################
+
+percentileStd <- function(M, percentile=0.03) {
+  # All-equal uncertainty interval from 3% percentile row sd
+  rep(quantile(apply(M,1,sd), percentile), nrow(M))
+}
+
+# Partition ranks by minimum regression error
+mse.f <- function(s,ss,n) ss - s**2/n
+adaptiveRegress <- function(v) {
+  x <- sort(v)
+  x2 <- x**2
+  k <- 0; mse <- Inf
+  sum.low <- 0; sqsum.low <- 0
+  sum.high <- sum(x); sqsum.high <- sum(x2)
+  n <- length(v)
+  stopifnot(n >= 2)
+
+  for (i in 1:(n-1)) {
+    sum.low <- sum.low + x[i]
+    sqsum.low <- sqsum.low + x2[i]
+    sum.high <- sum.high - x[i]
+    sqsum.high <- sqsum.high - x2[i]
+    mse.i <- mse.f(sum.low, sqsum.low, i) + mse.f(sum.high, sqsum.high, n-i)
+    if (mse.i < mse) {
+      k <- i; mse <- mse.i
+    }
+  }
+  thresh <- (x[k]+x[k+1])/2
+  list("thresh"=thresh, "k"=k)
+}
+
+logicClassMatrix <- function(M, thresholds=NULL, intervals=NULL, z=3, min.cnt=0, frac.conf=0.2) {
+  # All pairs rows.
+  if (is.null(thresholds)) {
+    thresholds <- apply(M, 1, function(v) adaptiveRegress(v)[[1]])
+  }
+  if (is.null(intervals)) {
+    intervals <- percentileStd(M)
+  }
+  # Quantize each row by low, uncertain, high
+  LUH <- matrix(0, nrow(M), ncol(M))
+  for (i in 1:nrow(LUH)) {
+    LUH[i,M[i,] < thresholds[i]-intervals[i]] <- -1
+    LUH[i,M[i,] > thresholds[i]+intervals[i]] <- 1
+  }
+  # Counts per quadrant: Q_{row}_{col}
+  QLL <- (LUH==-1) %*% t(LUH==-1) # row low, col low
+  QLH <- (LUH==-1) %*% t(LUH==1)  # row low, col high
+  QHL <- (LUH==1) %*% t(LUH==-1)  # row high, col low
+  QHH <- (LUH==1) %*% t(LUH==1)   # row high, col high
+
+  RL <- QLL + QLH               # row variable (y-axis) is low
+  RH <- QHL + QHH               # row variable (y-axis) is high
+  CL <- QLL + QHL               # col variable (x-axis) is low
+  CH <- QLH + QHH               # row variable (x-axis) is high
+  ALL <- QLL + QLH + QHL + QHH
+
+  # to avoid division by zero, set each margin to at least 1
+  ZERO.MARGIN = (RL==0)|(RH==0)|(CL==0)|(CH==0)
+  RL[RL==0] <- 1
+  RH[RH==0] <- 1
+  CL[CL==0] <- 1
+  CH[CH==0] <- 1
+  ALL[ALL==0] <- 1
+  TOO.FEW <- ALL < frac.conf*ncol(M)
+
+  # Dense/Sparse tests
+  SLL <- get.sparse(QLL, RL, CL, ALL, z=z, min.cnt=min.cnt)
+  SLH <- get.sparse(QLH, RL, CH, ALL, z=z, min.cnt=min.cnt)
+  SHL <- get.sparse(QHL, RH, CL, ALL, z=z, min.cnt=min.cnt)
+  SHH <- get.sparse(QHH, RH, CH, ALL, z=z, min.cnt=min.cnt)
+
+  # Assign class enumerations.
+  CLS <- matrix(0, nrow(M), nrow(M))
+  CLS[!SLL & SLH & !SHL & !SHH]  <- 1
+  CLS[!SLL & SLH & SHL & !SHH]   <- 2
+  CLS[!SLL & !SLH & SHL & !SHH]  <- 3
+  CLS[!SLL & !SLH & !SHL & !SHH] <- 4
+  CLS[!SLL & !SLH & !SHL & SHH]  <- 5
+  CLS[SLL & !SLH & !SHL & SHH]   <- 6
+  CLS[SLL & !SLH & !SHL & !SHH]  <- 7
+  CLS[ZERO.MARGIN] <- 0
+  CLS[TOO.FEW] <- 0
+  rownames(CLS) <- colnames(CLS) <- rownames(M)
+  CLS
+}
+
+get.sparse <- function(Q, Row, Col, All, z=3, min.cnt=0) {
+  Exp <- Row*Col/All
+  Z <- (Exp-Q) / sqrt(Exp)  # marginal probability test
+  D <- Q <= min.cnt         # number of points test
+  (Z > z) | D               # sparse matrix for single quadrant for all pairs
+}
---
+++ gsplom/R/util.R
@@ -0,0 +1,72 @@
+##########################################################################
+# Copyright (c) 2014 Andrew Yates
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+##########################################################################
+# --------------------------------------------------
+# Internal debugging tools to verify function.
+# --------------------------------------------------
+
+## compute sum squared error per step location "i"
+## 1:i -> low step
+## i+1:n -> high step
+fit.upstep <- function(v) {
+  R <- list()
+  v <- sort(v)
+  n <- length(v)
+  R$v.sse <- sapply(1:(n-1), function(i) sse(v[1:i])+sse(v[(i+1):n]))
+  R$idx <- which.min(R$v.sse)
+  R$sum.sse <- R$v.sse[R$idx]
+  R$th <- (v[R$idx]+v[R$idx+1])/2
+  R$low <- mean(v[1:R$idx])
+  R$high <- mean(v[(R$idx+1):n])
+  R$mean <- mean(v)
+  R$mean.idx <- tail(which(v<=mean(v)), 1)
+  R$median <- median(v)
+  R$median.idx <- max(floor(length(v)/2),1)
+  R
+}
+
+plot.sse <- function(R, add.mean.median=FALSE, ...) {
+  plot(R$v.sse, xlab="Highest Index In Low Step", ylab="Sum of Sum Squared Errors", ...)
+  legend("topright",pch=c(15,16,17),col=c("#cc0000","#0000cc","#009900"),legend=c("Stepfit","Mean","Median"))
+  abline(h=R$sum.sse, col="#cc0000", lty=1)
+  abline(v=R$idx, col="#cc0000", lty=3)
+  points(R$idx, R$v.sse[R$idx], col="#ff0000", pch=15, cex=3)
+  if(add.mean.median) {
+    # mean
+    abline(v=R$mean.idx, col="#0000cc", lty=3)
+    abline(h=R$v.sse[R$mean.idx], col="#0000cc", lty=1)
+    points(R$mean.idx, R$v.sse[R$mean.idx], col="#0000cc", pch=16, cex=3)
+    # median
+    abline(v=R$median.idx, col="#009900", lty=3)
+    abline(h=R$v.sse[R$median.idx], col="#009900", lty=1)
+    points(R$median.idx, R$v.sse[R$median.idx], col="#009900", pch=17, cex=3)    
+  }
+}
+
+plot.stepfit <- function(R, v, add.mean.median=FALSE, ...) {
+  v <- sort(v)
+  plot(v, xlab="Rank", ylab="Log Expression", ...)
+  legend("topleft",pch=c(15,16,17),col=c("#cc0000","#0000cc","#009900"),legend=c("Stepfit","Mean","Median"))
+  abline(h=R$th, col="#cc0000", lty=2, lwd=2)
+  abline(v=R$idx, col="#990000", lty=3)
+  abline(h=R$high, col="#990000", lwd=1, lty=3)
+  abline(h=R$low, col="#990000", lwd=1, lty=3)
+  if(add.mean.median) {
+    abline(h=R$mean, col="#0000cc", lty=2, lwd=2)
+    #abline(v=R$mean.idx, col="#0000cc", lty=3, lwd=1)
+    points(R$mean.idx, R$mean, col="#0000cc", pch=16, cex=3)
+    abline(h=R$median, col="#009900", lty=2, lwd=2)
+    #abline(v=R$median.idx, col="#009900", lty=3, lwd=1)
+    points(R$median.idx, R$median, col="#009900", pch=17, cex=3)
+  }
+  segments(x0=0, y0=R$low, x1=R$idx, y1=R$low, col="#ff0000", lwd=6)
+  segments(x0=R$idx, y0=R$low, x1=R$idx, y1=R$high, col="#ff0000", lwd=6)
+  segments(x0=R$idx, y0=R$high, x1=length(v)+1, y1=R$high, col="#ff0000", lwd=6)
+  points(R$idx+0.5, R$th, col="#ff0000", pch=15, cex=3)
+}
