40ed529e0be1e872629aa07c68c2a026bac15278
Andrew Yates <andrewyates.name@gmail.com>
2014-03-12 03:11:08 -0700

    documentation update

--- gsplom/vignettes/gsplom.vignette.R
+++
@@ -1,8 +0,0 @@
-### R code from vignette source 'SCAN.vignette.Rnw'
-### Encoding: UTF-8
-
-###################################################
-### code chunk number 1: setup
-###################################################
-## do work in temporary directory
-pwd <- setwd(tempdir())
