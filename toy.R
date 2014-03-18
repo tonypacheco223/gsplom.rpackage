4a72133e4b1daf995ce27b71d1045db553d1adeb
Andrew Yates <andrewyates.name@gmail.com>
2014-03-18 14:06:23 -0700

    Update toy.R

--- toy.R
+++ toy.R
@@ -7,6 +7,12 @@ CLS.Dist <- logicClassDist(CLS)
 DCOR.Dist <- as.matrix(dist(DCOR))
 DIST <- DCOR.Dist + sqrt(CLS.Dist)/2
 
+# vector with labels
+pdf("~/Desktop/pix_glyph.pdf", width=10, height=10)
+gsplom(M)
+dev.off()
+
+# rasterized, pixel perfect images
 png("~/Desktop/pix_glyph.png", nrow(M), nrow(M))
 par(mar = c(0,0,0,0))
 gsplom(M, doLabels=FALSE, useRaster=TRUE)
